4deedea83769ff449c7bf85253594ab11eba46f1
blkZ <1135065316@qq.com>
2020-10-17 16:09:44 +0800

    删除了无用页面

---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+		123
+  </div>
+</template>
+
+<script>
+
+export default {
+	name: 'Test',
+  data() {
+		return {
+			key: 111
+		}
+	},
+	onReady() {
+		console.log(1)
+	}
+}
+</script>
